0764de22fd714099c2e04e09a621818ba637f59c
Ryan Lane <ryan.lane@kellogg.northwestern.edu>
2017-02-26 15:15:00 -0600

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+var React = require('react')
+
+var Header = React.createClass({
+  greeting: function() {
+    if (this.props.currentUser) {
+      return (
+        <span>
+          Hi {this.props.currentUser.displayName}
+          &nbsp;
+          <a href="#" className="btn btn-link" onClick={this.props.logout}>
+            <i className="fa fa-sign-out"></i>
+          </a>
+        </span>
+      )
+    } else {
+      return (
+        <a href="#" className="btn btn-link" onClick={this.props.login}>Login</a>
+      )
+    }
+  },
+  render: function() {
+    return (
+      <div className="header row">
+        <div className="col-sm-9">
+          <h1>Buyflix</h1>
+        </div>
+        <div className="hello col-sm-3 text-center">
+          <h2>{this.greeting()}</h2>
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Header;
